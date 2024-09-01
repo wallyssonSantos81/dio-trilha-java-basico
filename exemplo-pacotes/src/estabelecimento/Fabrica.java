38aa4f8eb7b4975d1f9d917a40fbadd277713c5a
Wallisson Santos <wallyssonsantos@yahoo.com.br>
2024-09-01 12:51:19 -0300

    Commit estudo de Interface

--- exemplo-pacotes/src/estabelecimento/Fabrica.java
+++ exemplo-pacotes/src/estabelecimento/Fabrica.java
@@ -9,9 +9,6 @@ import equipamentos.multifuncional.EquipamentoMultiFuncional;
 
 public class Fabrica {
 	public static void main(String[] args) {
-		//Impressora impressora = new Deskjet();
-		//Impressora impressora = new Laserjet();
-		//Impressora impressora = new EquipamentoMultiFuncional();
 		EquipamentoMultiFuncional em = new EquipamentoMultiFuncional();
 		Impressora impressora = em;
 		Digitalizadora digitalizadora = em;
