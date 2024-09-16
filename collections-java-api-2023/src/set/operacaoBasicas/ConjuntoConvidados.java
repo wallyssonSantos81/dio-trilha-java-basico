138460337a56ec880a28d75c32441a139c7baf59
Wallisson Santos <wallyssonsantos@yahoo.com.br>
2024-09-15 22:25:15 -0300

    Commit estudo sodre Set na plataforma DIO

---
+++ collections-java-api-2023/src/set/operacaoBasicas/ConjuntoConvidados.java
@@ -0,0 +1,51 @@
+package set.operacaoBasicas;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoConvidados {
+    //Atributos
+
+    private Set<Convidado> convidadoSet;
+
+    public ConjuntoConvidados() {
+        this.convidadoSet = new HashSet<>();
+    }
+    public void adicionarConvidado(String nome, int codigoConvite){
+        convidadoSet.add(new Convidado(nome, codigoConvite));
+    }
+    public void removerConvidadoPorCodigoConvite(int codigoConvite){
+        Convidado convidadoParaRemover = null;
+        for(Convidado c: convidadoSet){
+            if(c.getCodigoConvite() == codigoConvite){
+                convidadoParaRemover = c;
+                break;
+            }
+
+        }
+        convidadoSet.remove(convidadoParaRemover);
+    }
+    public int contarConvidados(){
+        return convidadoSet.size();
+    }
+    public void exibirConvidados(){
+        System.out.println(convidadoSet);
+    }
+    public static void main(String[] args) {
+        ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " dentro do Set convidados");
+
+        conjuntoConvidados.adicionarConvidado("Convidado 1", 1234);
+        conjuntoConvidados.adicionarConvidado("Convidado 2", 1235);
+        conjuntoConvidados.adicionarConvidado("Convidado 3", 1236);
+        conjuntoConvidados.adicionarConvidado("Convidado 4", 1237);
+
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidado(a) dentro do Set convidados");
+
+        conjuntoConvidados.removerConvidadoPorCodigoConvite(1234);
+
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " dentro do Set convidados");
+
+    }
+
+}
