b2c199a8935c5915143c78e27bf8cb77f2621057
Wallisson Santos <wallyssonsantos@yahoo.com.br>
2024-09-19 06:47:16 -0300

    Commit estudo de Map com exercicios da plataforma DIO

---
+++ collections-java-api-2023/src/map/pesquisa/Produto.java
@@ -0,0 +1,36 @@
+package map.pesquisa;
+
+public class Produto {
+     //atributos
+  private String nome;
+  private double preco;
+  private int quantidade;
+
+  public Produto(String nome, double preco, int quantidade) {
+    this.nome = nome;
+    this.preco = preco;
+    this.quantidade = quantidade;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public double getPreco() {
+    return preco;
+  }
+
+  public int getQuantidade() {
+    return quantidade;
+  }
+
+  @Override
+  public String toString() {
+    return "Produto{" +
+        "nome='" + nome + '\'' +
+        ", preco=" + preco +
+        ", quantidade=" + quantidade +
+        '}';
+  }
+
+}
