e51c38cc453658a5af2da44f87c7deadee429fa6
Wallisson Santos <wallyssonsantos@yahoo.com.br>
2024-10-18 14:17:34 -0300

    Commit estudo Spring boot

---
+++ primeiros-passos/src/main/java/dio/springboot/Calculadora.java
@@ -0,0 +1,11 @@
+package dio.springboot;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Calculadora {
+    public int somar(int numero1, int numero2){
+        return numero1 + numero2;
+    }
+
+}
